70513e5cb9d83bbac2812b7919ea18242e773873
Stefan Hoffmann <stho32github@gmail.com>
2020-11-29 11:10:00 +0100

    get unit tests to work

--- Source/WebsiteValidator.BL.Tests/Class1.cs
+++ Source/WebsiteValidator.BL.Tests/Class1.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace WebsiteValidator.BL.Tests
 {
     public class Class1
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Class1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            _testOutputHelper.WriteLine("Hello world");
+        }
     }
 }
