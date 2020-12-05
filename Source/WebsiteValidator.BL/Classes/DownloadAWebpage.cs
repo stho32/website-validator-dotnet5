379116ff0b139ff597d484693d80560d8278520c
naseif <d2mds21@gmail.com>
2020-12-05 12:15:15 +0100

    Added new parameter for ignoring ssl certificates

--- Source/WebsiteValidator.BL/Classes/DownloadAWebpage.cs
+++ Source/WebsiteValidator.BL/Classes/DownloadAWebpage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WebsiteValidator.BL.Interfaces;
@@ -7,12 +8,37 @@ namespace WebsiteValidator.BL.Classes
 {
     public class DownloadAWebpage : IDownloadAWebpage
     {
+        private bool ignoreSsl;
+
+        public DownloadAWebpage(bool ignoreSsl)
+        {
+            this.ignoreSsl = ignoreSsl;
+        }
+
+        public bool ForceBadCertificate { get; set; }
+
         public async Task<IWebpage> Download(string url)
         {
-            HttpClient client = new HttpClient();
-            var response = await client.GetAsync(url);
-            var pageContents = await response.Content.ReadAsStringAsync();
-            return new Webpage(url, pageContents, response.StatusCode);
+            using (var httpClientHandler = new HttpClientHandler())
+            {
+                if (ignoreSsl)
+                {
+                    httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                }
+
+                if (ForceBadCertificate && !ignoreSsl)
+                {
+                    httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return false; };
+                }
+
+                using (var client = new HttpClient(httpClientHandler))
+                {
+                    // Make request here.
+                    var response = await client.GetAsync(url);
+                    var pageContents = await response.Content.ReadAsStringAsync();
+                    return new Webpage(url, pageContents, response.StatusCode);
+                }
+            }
         }
     }
 }
