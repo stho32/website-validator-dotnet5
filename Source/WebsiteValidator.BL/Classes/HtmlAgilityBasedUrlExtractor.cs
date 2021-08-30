367101e14140327632f1b415af0c594ee003f729
stho32 <stho32github@gmail.com>
2021-08-30 14:04:07 +0200

    also add links from img src to the analysed bunch

--- Source/WebsiteValidator.BL/Classes/HtmlAgilityBasedUrlExtractor.cs
+++ Source/WebsiteValidator.BL/Classes/HtmlAgilityBasedUrlExtractor.cs
@@ -6,25 +6,35 @@ namespace WebsiteValidator.BL.Classes
 {
     public class HtmlAgilityBasedUrlExtractor : IUrlExtractor
     {
-        public string[] ExtractUrls(string pageContents)
-        {
+    
+        private string[] ExtractUrlsByTagAndAttribute(string pageContents, string tag, string attribute) {
             var result = new List<string>();
 
             var document = new HtmlDocument();
             document.LoadHtml(pageContents);
-            var linkNodes = document.DocumentNode.SelectNodes("//a[@href]");
+            var linkNodes = document.DocumentNode.SelectNodes($"//{tag}[@{attribute}]");
 
             if (linkNodes != null)
             {
                 foreach (var link in linkNodes)
                 {
-                    var linkUrl = link.GetAttributeValue("href", "");
+                    var linkUrl = link.GetAttributeValue(attribute, "");
                     if (!string.IsNullOrWhiteSpace(linkUrl))
                     {
                         result.Add(linkUrl);
                     }
                 }
             }
+
+            return result.ToArray();
+        }
+    
+        public string[] ExtractUrls(string pageContents)
+        {
+            var result = new List<string>();
+
+            result.AddRange(ExtractUrlsByTagAndAttribute(pageContents, "a", "href"));
+            result.AddRange(ExtractUrlsByTagAndAttribute(pageContents, "img", "src"));
 
             return result.ToArray();
         }
